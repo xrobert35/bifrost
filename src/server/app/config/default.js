138a73e880ae63ae88443dfb7b69d575a5f82746
xrobert35 <ixoh35@gmail.com>
2020-04-04 23:16:40 +0200

    fix(conf): fix boolean configuration parameters

--- src/server/app/config/default.js
+++ src/server/app/config/default.js
@@ -1,11 +1,11 @@
 const config = {
   SERVER_PORT: process.env.SERVER_PORT || '4000',
   CLIENT_PORT: process.env.CLIENT_PORT || '4080',
-  CLIENT_ACTIVATED: process.env.CLIENT_ACTIVATED || true,
-  CLIENT_PROXY_ACTIVATED: process.env.CLIENT_PROXY_ACTIVATED || true,
+  CLIENT_ACTIVATED: process.env.CLIENT_ACTIVATED != null ? process.env.CLIENT_PROXY_ACTIVATED : true,
+  CLIENT_PROXY_ACTIVATED: process.env.CLIENT_PROXY_ACTIVATED != null ? process.env.CLIENT_PROXY_ACTIVATED : true,
   SERVER_PATH : process.env.SERVER_PATH || '/api',
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
-  SWAGGER_ACTIVATED: process.env.SWAGGER_ACTIVATED || false,
+  SWAGGER_ACTIVATED: process.env.SWAGGER_ACTIVATED != null ? process.env.SWAGGER_ACTIVATED : false,
   DOCKER_REPO_KEYS : process.env.DOCKER_REPO_KEYS || null,
   SERVER_DATA: process.env.SERVER_DATA || '/opt/bifrost',
   DEFAULT_COMPOSE_FOLDER : process.env.DEFAULT_COMPOSE_FOLDER || '/opt/docker/compose'
--- src/server/app/config/development.js
+++ src/server/app/config/development.js
@@ -1,7 +1,7 @@
 const config = {
   LOG_LEVEL: process.env.LOG_LEVEL || "debug",
-  SWAGGER_ACTIVATED: process.env.SWAGGER_ACTIVATED || true,
-  CLIENT_ACTIVATED: process.env.CLIENT_ACTIVATED || false,
-  CLIENT_PROXY_ACTIVATED: process.env.CLIENT_PROXY_ACTIVATED || true,
+  SWAGGER_ACTIVATED: process.env.SWAGGER_ACTIVATED != null ? process.env.SWAGGER_ACTIVATED : true,
+  CLIENT_ACTIVATED: process.env.CLIENT_ACTIVATED != null ? process.env.CLIENT_ACTIVATED : false,
+  CLIENT_PROXY_ACTIVATED: process.env.CLIENT_PROXY_ACTIVATED != null ? process.env.CLIENT_PROXY_ACTIVATED : true,
 };
 module.exports = { config: config }
